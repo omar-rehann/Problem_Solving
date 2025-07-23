5a0ed262501f63cfd9cc69f7c13387ed45a43753
Omar Rehan <omarrehan724@gmail.com>
2025-07-23 17:33:22 +0300

    One Questiom

--- Kat 00_10/even_odd_number.js
+++ Kat 00_10/even_odd_number.js
@@ -1,1 +1,6 @@
 // one problem
+// Write a function that takes an integer as input and returns a string indicating whether the number is even or odd.
+function Even_number(number) {
+    return number % 2 === 0 ? " Even Number " : "Odd Number";
+}
+console.log(Even_number(1))
