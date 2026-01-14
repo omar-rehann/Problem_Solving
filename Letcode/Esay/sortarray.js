9ad5c08c6a62eb01a48729d6769abb225050daca
Omar Rehan <omarrehan724@gmail.com>
2026-01-14 20:14:04 +0200

    add new problem in essay

---
+++ Letcode/Esay/sortarray.js
@@ -0,0 +1,14 @@
+/*
+Sort an Array
+
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions,
+in O(n log n) time complexity and with the smallest space complexity possible
+
+*/
+function sortedarray(arr) {
+    let test = arr.sort();
+    return test;
+}
+console.log(sortedarray([2, 3, 5, 0, 1]))
