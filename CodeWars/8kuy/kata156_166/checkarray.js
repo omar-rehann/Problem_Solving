f72dd3c75485a2256ceb46f753f7a8abceb47982
Omar Rehan <omarrehan724@gmail.com>
2026-03-11 20:11:54 +0200

    add new commit

---
+++ CodeWars/8kuy/kata156_166/checkarray.js
@@ -0,0 +1,17 @@
+/*
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+a can contain numbers or strings. x can be either.
+
+Return true if the array contains the value, false if not.
+
+*/
+function check(a, x) {
+    if (a.includes(x)) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(check(["a", "b", "c"], "d"))
