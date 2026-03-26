bbe77a95cce19a22787999ea97f02b6eb9a2b6d4
Omar Rehan <omarrehan724@gmail.com>
2026-03-26 18:43:06 +0200

    add new problem

---
+++ CodeWars/8kuy/kata156_166/polygin.js
@@ -0,0 +1,10 @@
+/*
+
+Write the function polygonArea(A,B,C,D) 
+that finds the area of polygons of this type:
+*/
+function polygonArea(A, B, C, D) {
+    return (A + C) / 2 * B;
+}
+
+console.log(polygonArea(2, 2, 2, 2))
