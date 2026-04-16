9200c206a19320546efddb86ce326ba6f420d7ba
Omar Rehan <omarrehan724@gmail.com>
2026-04-16 19:35:19 +0200

    add new problem

---
+++ CodeWars/7kuy/kata89_99/cupe.js
@@ -0,0 +1,9 @@
+/*
+Calculate the sum of the numbers in the nth row of 
+this triangle (starting at index 1) e.g.: (Input --> Output)
+
+*/
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+}
+console.log(rowSumOddNumbers(2))
