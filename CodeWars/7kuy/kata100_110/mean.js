66601b12d04408acbc21a3885700c525c5a2eb88
Omar Rehan <omarrehan724@gmail.com>
2026-05-04 11:51:40 +0300

    add new problem

---
+++ CodeWars/7kuy/kata100_110/mean.js
@@ -0,0 +1,30 @@
+/*
+Description:
+You will be given an array which will include
+both integers and characters.
+Return an array of length 2 with a[0] representing the mean
+of the ten integers as a floating point number.
+There will always be 10 integers and 10 characters.
+Create a single string with the characters and return 
+it as a[1] while maintaining 
+the original order.
+lst = ['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']
+*/
+function mean(lst) {
+    let lengthnumber = lst.filter(function(e) {
+        return !isNaN(e);
+    })
+    let allnumber = lst.filter(function(e) {
+        return !isNaN(e);
+    }).reduce(function(one, two) {
+        return +one + +two;
+    });
+    let allstring = lst.filter(function(e) {
+        return isNaN(e)
+    }).reduce(function(one, two) {
+        return one + two;
+    })
+    return [(allnumber / lengthnumber.length), allstring];
+
+}
+console.log(mean(['u', '6', 'd', '1', 'i', 'w', '6', 's', 't', '4', 'a', '6', 'g', '1', '2', 'w', '8', 'o', '2', '0']))
