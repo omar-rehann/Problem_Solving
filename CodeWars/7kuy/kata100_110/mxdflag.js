37726d9f6cb01b723e3766374e2323fb6102a751
Omar Rehan <omarrehan724@gmail.com>
2026-05-12 12:27:28 +0300

    add new problem

---
+++ CodeWars/7kuy/kata100_110/mxdflag.js
@@ -0,0 +1,29 @@
+/*
+
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+Find max(abs(length(x) − length(y)))
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+Example:
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+Bash note:
+input : 2 strings with substrings separated by ,
+output: number as a string
+*/
+function mxdiflg(a1, a2) {
+    let test = []
+    if (a1.length === 0 || a2.length === 0) {
+        return -1
+    } else {
+        for (let i = 0; i < a1.length; i++) {
+            for (let j = 0; j < a2.length; j++) {
+                test.push(Math.abs(a1[i].length - a2[j].length))
+            }
+        }
+        return test.reduce(function(one, two) {
+            return one > two ? one : two
+        });
+    }
+}
+console.log(mxdiflg(["aaa", "bb"], ["C", "dddddd"]))
