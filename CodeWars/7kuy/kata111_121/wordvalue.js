d7ddabfc6ccff64799cf12da84a2adae4e4a36e3
Omar Rehan <omarrehan724@gmail.com>
2026-05-21 12:41:14 +0300

    add new problem

---
+++ CodeWars/7kuy/kata111_121/wordvalue.js
@@ -0,0 +1,44 @@
+/*
+
+Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ... z = 26.
+
+You will be given a list of strings and your task will be to return the values of the strings as explained above multiplied by the position of that string in the list. For our purpose, position begins with 1.
+
+["abc", "abc abc"] should return [6, 24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+
+"abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied by 1 while the value at position 2 is multiplied by 2.
+
+Input will only contain lowercase characters and spaces.
+
+Good luck!
+
+If you like this Kata, please try:
+
+String matchup
+
+Consonant value
+
+*/
+function wordValue(words) {
+    let sum = [];
+
+    for (let i = 0; i < words.length; i++) {
+
+        let chars = words[i].split("");
+        let total = 0;
+
+        for (let j = 0; j < chars.length; j++) {
+
+            if (chars[j] !== " ") {
+                total += chars[j].charCodeAt(0) - 96;
+            }
+
+        }
+
+        sum.push(total * (i + 1));
+    }
+
+    return sum;
+}
+
+console.log(wordValue(["abc", "abc abc"]));
