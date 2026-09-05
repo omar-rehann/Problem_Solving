0b771bdc0a57db04f6201c4820eaa42f64383205
Omar Rehan <omarrehan724@gmail.com>
2026-09-05 19:36:15 +0300

    add new problem

---
+++ Revison/1 week/eight.js
@@ -0,0 +1,14 @@
+/*
+
+
+1. Reversed sequenceBuild a
+
+function that returns an array of integers from n to 1 where $n > 0 $.
+Example:
+    n = 5 => [5, 4, 3, 2, 1] *
+*/
+function Reversed(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i)
+    }
+}
