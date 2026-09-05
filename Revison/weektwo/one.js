41665bc412428ade83ff6597d55a1cf083a3af4b
Omar Rehan <omarrehan724@gmail.com>
2026-09-05 19:38:59 +0300

    add new problem

---
+++ Revison/weektwo/one.js
@@ -0,0 +1,12 @@
+/*
+
+4. Convert a Number to a String!We need a function that can transform a number (integer) into a string.
+Examples (input --> output):
+123 --> "123"
+999 --> "999"
+-100 --> "-100"
+
+*/
+function converts(n) {
+    return String(n);
+}
