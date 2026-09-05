a44a8c2bc0fc5d8e73f57fe5158fc6986abc6889
Omar Rehan <omarrehan724@gmail.com>
2026-09-05 19:40:01 +0300

    add new problem

---
+++ Revison/weektwo/two.js
@@ -0,0 +1,15 @@
+/*
+5. Invert valuesGiven a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.  
+Examples:  
+invert([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
+invert([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
+invert([]) == []
+(You can assume that all values are integers. Do not mutate the input array/list.)
+
+*/
+function invert(nums) {
+    let test = nums.map((e) => {
+        return e > 0 ? -e : e
+    })
+    return test
+}
