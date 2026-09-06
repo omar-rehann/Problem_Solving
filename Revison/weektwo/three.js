be9894c7903596b78a00ed5eeeaec4fff363b5e9
Omar Rehan <omarrehan724@gmail.com>
2026-09-06 19:00:13 +0300

    add new problem

---
+++ Revison/weektwo/three.js
@@ -0,0 +1,12 @@
+/*
+
+ 1. Reversed sequenceBuild a
+
+function that returns an array of integers from n to 1 where $n > 0 $.
+Example:
+    n = 5 => [5, 4, 3, 2, 1]
+
+*/
+function reversed(n) {
+    return ["one", "two", "three", "four", "five"].length
+}
