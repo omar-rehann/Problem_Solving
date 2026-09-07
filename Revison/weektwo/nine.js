61b97cdcb295e31001996d59db06314705144ab3
Omar Rehan <omarrehan724@gmail.com>
2026-09-07 20:29:36 +0300

    add new problem

---
+++ Revison/weektwo/nine.js
@@ -0,0 +1,10 @@
+/*
+
+
+Function 3 - multiplying two numbers
+Instructions: Implement a function called multiply, which takes two numbers and returns their product.
+
+*/
+function multiply(a, b) {
+    return a * b;
+}
