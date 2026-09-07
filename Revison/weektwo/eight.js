e44bb238dbb20325676dada765b2bf334f51e229
Omar Rehan <omarrehan724@gmail.com>
2026-09-07 20:28:46 +0300

    add new problem

---
+++ Revison/weektwo/eight.js
@@ -0,0 +1,9 @@
+/*
+
+Without numbers!
+Instructions: Write a function that always returns 5, without using any of the following characters: 0123456789*+-/
+
+*/
+function unbreakableFive() {
+    return "fivea".length;
+}
