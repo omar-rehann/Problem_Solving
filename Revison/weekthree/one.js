94768b933a8c49c9e0048264b5b1de5aa8dc2205
Omar Rehan <omarrehan724@gmail.com>
2026-09-07 20:30:54 +0300

    add new problem

---
+++ Revison/weekthree/one.js
@@ -0,0 +1,9 @@
+/*
+Is n divisible by x and y?
+Instructions: Create a function that checks if a number n is divisible by two numbers x AND y.
+
+*/
+Is n divisible by x and y ?
+    Instructions : Create a
+function that checks
+if a number n is divisible by two numbers x AND y.
