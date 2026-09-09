808e60147e12838c2f91ecc2ef1eec02bccfd990
Omar Rehan <omarrehan724@gmail.com>
2026-09-09 13:41:16 +0300

    add new problem

---
+++ Revison/weekthree/six.js
@@ -0,0 +1,10 @@
+/*
+
+2. Function 3 - multiplying two numbersInstructions:
+Implement a function called multiply, which takes two numbers and returns their product:
+multiply(2, 3) = 6multiply(0, 188) = 0multiply(85, 144) = 12240
+
+*/
+function multiply(x, y) {
+    return x * y
+}
