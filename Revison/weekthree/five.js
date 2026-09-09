e0c0daacd463fc845686bbb1f14ed12b2fb2191b
Omar Rehan <omarrehan724@gmail.com>
2026-09-09 13:40:18 +0300

    add new problem

---
+++ Revison/weekthree/five.js
@@ -0,0 +1,11 @@
+/*
+
+
+1. 5 without numbers !!Instructions:
+Write a function that always returns 5
+Sounds easy right? Just bear in mind that you can't use any of the following characters:0123456789*+-/
+Good luck :)
+*/
+function Instructions(n) {
+    return ["one", "two", "three", "four", "five"].length
+}
