c0a2838982e2131d8f508d54d5d3a0ac62c3c38b
Omar Rehan <omarrehan724@gmail.com>
2026-09-10 15:17:26 +0300

    add new problem

---
+++ Revison/weekfour/one.js
@@ -0,0 +1,10 @@
+/*
+
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+P.S. Each array includes only integer numbers. Output is a number too.
+
+*/
+function coding(arr1, arr2) {
+    let test = arr1.conact(arr2);
+    return test;
+}
---
+++ Revison/weekthree/ten.js
@@ -0,0 +1,7 @@
+/*
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+function removes(n) {
+    return String(n).replace("!", "")
+}
