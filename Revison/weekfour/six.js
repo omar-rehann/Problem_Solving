6ccabe58beed26b064c3ce9285d11411209aa72a
Omar Rehan <omarrehan724@gmail.com>
2026-09-13 14:40:58 +0300

    add new problem

---
+++ Revison/weekfour/six.js
@@ -0,0 +1,17 @@
+/*
+
+2.   (Sum The Strings):Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+Example: (Input1, Input2 --> Output)
+"4", "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+*/
+function sum(a, b) {
+    if (a == "" && b == "") {
+        return "0";
+    } else {
+        return parseInt(a) + parseInt(b)
+    }
+}
