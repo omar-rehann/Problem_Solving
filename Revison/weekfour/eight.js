254530e0aa7e5aaf1ebff89c31d051fcc92608eb
Omar Rehan <omarrehan724@gmail.com>
2026-09-14 15:41:55 +0300

    add new edit

---
+++ Revison/weekfour/eight.js
@@ -0,0 +1,9 @@
+/*
+
+ : Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+function Convert(v) {
+    return toString(v) ? true : false
+}
