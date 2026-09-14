7e45ea218e81e15009bf683abc7444f0977283df
Omar Rehan <omarrehan724@gmail.com>
2026-09-14 15:43:17 +0300

    add new edit

---
+++ Revison/weekfour/nine.js
@@ -0,0 +1,9 @@
+/*
+
+  Even or Odd
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+function check(n) {
+    return n % 2 === 0 ? true : false
+}
