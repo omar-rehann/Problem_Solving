00ddd1091de57ebfe8acea6c35e3c44b4e3edd32
Omar Rehan <omarrehan724@gmail.com>
2026-09-16 15:32:00 +0300

    add new problem

---
+++ Revison/weekfive/one.js
@@ -0,0 +1,6 @@
+/*
+
+It's the academic year's end, fate has decide to be in your favor, the marks you got are written in the paper. But your teacher has lost your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.  
+Return the average of the given array rounded down to its nearest integer.  
+The array will never be empty.
+*/
