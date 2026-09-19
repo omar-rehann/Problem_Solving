7102204af2052ed6bd0f916dfc0f650ccdd00701
Omar Rehan <omarrehan724@gmail.com>
2026-09-19 15:52:30 +0300

    revion codewars

---
+++ Revison/weekfive/five.js
@@ -0,0 +1,10 @@
+/*
+Complete the solution so that it reverses the string passed into it.   
+Examples:'world' => 'dlrow'
+   
+'word' => 'drow'
+
+*/
+function reverses(s) {
+    let test = Array.from(s).reverse().join('');
+}
